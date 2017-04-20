329363fc67307b77483db022cdfa1bd449ba630e
gonzalo <gonzalo@gonzalo-Lenovo-B50-70>
2017-04-20 20:42:11 -0300

    Mejor modulado el javascript para las distintas páginas. Añadido el control js para el selector de forma.

---
+++ public_html/js/creacion.js
@@ -0,0 +1,71 @@
+// PANEL DE CREACIÓN DE ALMOHADÓN
+
+function actualizarAlmohadon() {
+    var newSRC = baseSRC + almohadonNuevo.modelo + "_" + almohadonNuevo.forma + "_" + almohadonNuevo.color + ".jpg";
+    $("#almohadon").attr("src", newSRC);
+}
+
+function forma() {
+    $("#selectorForma").change(function () {
+        almohadonNuevo.forma = $("#selectorForma option:selected").text().toLowerCase();
+        actualizarAlmohadon();
+        console.log(almohadonNuevo.forma);
+    });
+}
+
+function botonesColores() {
+    $("#botonRojo").click(function () {
+        almohadonNuevo.color = "rojo";
+        actualizarAlmohadon();
+    });
+
+    $("#botonAzul").click(function () {
+        almohadonNuevo.color = "azul";
+        actualizarAlmohadon();
+    });
+
+    $("#botonVerde").click(function () {
+        almohadonNuevo.color = "verde";
+        actualizarAlmohadon();
+    });
+
+    $("#botonBeige").click(function () {
+        almohadonNuevo.color = "beige";
+        actualizarAlmohadon();
+    });
+
+    $("#botonRosa").click(function () {
+        almohadonNuevo.color = "rosa";
+        actualizarAlmohadon();
+    });
+
+    $("#botonVioleta").click(function () {
+        almohadonNuevo.color = "violeta";
+        actualizarAlmohadon();
+    });
+}
+
+var baseSRC = "img/almohadones/";
+
+var almohadonNuevo = {
+    "modelo": "botoncentral",
+    "forma": "cuadrado",
+    "color": "rojo"
+};
+
+//Almohadón por defecto
+
+function setAlmohadonPorDefecto() {
+    almohadonNuevo.modelo = "liso";
+    almohadonNuevo.forma = "cuadrado";
+    almohadonNuevo.color = "azul";
+    actualizarAlmohadon();
+}
+
+$(document).ready(function () {
+    cargarEstilo();
+    botonesColores();
+    seleccionTema();
+    setAlmohadonPorDefecto();
+    forma();
+});
