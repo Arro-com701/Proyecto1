e35f0d238c7e78811822920ed3226cc55087a54f
gonzalo <gonzalo@gonzalo-Lenovo-B50-70>
2017-04-21 02:17:31 -0300

    Imagenes para el tercer modelo. Agregue funciones para el presupuesto.

--- public_html/js/creacion.js
+++ public_html/js/creacion.js
@@ -5,11 +5,73 @@ function actualizarAlmohadon() {
     $("#almohadon").attr("src", newSRC);
 }
 
+function actualizarPrecio() {
+    var total = actualizarPrecioForma()+actualizarPrecioModelo()+actualizarPrecioColor();
+    $("#precioTotal").text("$"+total);
+}
+
+function actualizarPrecioForma() {
+    if (almohadonNuevo.forma === "cuadrado") {
+        $("#precioForma").text("$0");
+        return 0;
+    }
+    if (almohadonNuevo.forma === "curvo") {
+        $("#precioForma").text("$20");
+        return 20;
+    }
+    if (almohadonNuevo.forma === "rectangular") {
+        $("#precioForma").text("$10");
+        return 10;
+    }
+}
+
+function actualizarPrecioModelo() {
+    if (almohadonNuevo.modelo === "modelo1") {
+        $("#precioModelo").text("$140");
+        return 140;
+    }
+    if (almohadonNuevo.modelo === "modelo2") {
+        $("#precioModelo").text("$160");
+        return 160;
+    }
+    if (almohadonNuevo.modelo === "modelo3") {
+        $("#precioModelo").text("$185");
+        return 185;
+    }
+}
+
+function actualizarPrecioColor() {
+    if (almohadonNuevo.color === "azul") {
+        $("#precioColor").text("$10");
+        return 10;
+    }
+    if (almohadonNuevo.color === "rojo") {
+        $("#precioColor").text("$10");
+        return 10;
+    }
+    if (almohadonNuevo.color === "verde") {
+        $("#precioColor").text("$20");
+        return 20;
+    }
+    if (almohadonNuevo.color === "violeta") {
+        $("#precioColor").text("$20");
+        return 20;
+    }
+    if (almohadonNuevo.color === "beige") {
+        $("#precioColor").text("$5");
+        return 5;
+    }
+    if (almohadonNuevo.color === "rosa") {
+        $("#precioColor").text("$10");
+        return 10;
+    }
+}
+
 function forma() {
     $("#selectorForma").change(function () {
         almohadonNuevo.forma = $("#selectorForma option:selected").text().toLowerCase();
         actualizarAlmohadon();
-        console.log(almohadonNuevo.forma);
+        actualizarPrecio();
     });
 }
 
@@ -17,39 +79,47 @@ function modelo() {
     $(".modal a").click(function () {
         almohadonNuevo.modelo = $(this).attr("id");
         actualizarAlmohadon();
+        actualizarPrecio();
         $('#myModal').modal('hide');
     });
 }
 
+//RESUMIR ESTO ASI PUEDO ESCRIBIR UNA FUNCION SOLA
 function botonesColores() {
     $("#botonRojo").click(function () {
         almohadonNuevo.color = "rojo";
         actualizarAlmohadon();
+        actualizarPrecio();
     });
 
     $("#botonAzul").click(function () {
         almohadonNuevo.color = "azul";
         actualizarAlmohadon();
+        actualizarPrecio();
     });
 
     $("#botonVerde").click(function () {
         almohadonNuevo.color = "verde";
         actualizarAlmohadon();
+        actualizarPrecio();
     });
 
     $("#botonBeige").click(function () {
         almohadonNuevo.color = "beige";
         actualizarAlmohadon();
+        actualizarPrecio();
     });
 
     $("#botonRosa").click(function () {
         almohadonNuevo.color = "rosa";
         actualizarAlmohadon();
+        actualizarPrecio();
     });
 
     $("#botonVioleta").click(function () {
         almohadonNuevo.color = "violeta";
         actualizarAlmohadon();
+        actualizarPrecio();
     });
 }
 
@@ -64,7 +134,7 @@ var almohadonNuevo = {
 //Almohadón por defecto
 
 function setAlmohadonPorDefecto() {
-    almohadonNuevo.modelo = "liso";
+    almohadonNuevo.modelo = "modelo1";
     almohadonNuevo.forma = "cuadrado";
     almohadonNuevo.color = "azul";
     actualizarAlmohadon();
---
+++ public_html/js/tema.js
@@ -0,0 +1,40 @@
+// ESTILO DE LA PÁGINA
+
+/*
+ * Carga el último estilo seleccionado (recordado por el browser).
+ */
+function cargarEstilo() {
+    // Los valores en setEstilo son por la ubicación de las css
+    // en index.html
+    if (localStorage.tema === "Tema 1")
+        setEstilo(3);
+    else
+        setEstilo(2);
+}
+
+/*
+ * Deshabilita los dos estilos existentes para el tema de la página.
+ */
+function deshabilitarEstilos() {
+    document.styleSheets[2].disabled = true;
+    document.styleSheets[3].disabled = true;
+}
+
+/*
+ * Setea el estilo correspondiente al parámetro.
+ */
+function setEstilo(n) {
+    deshabilitarEstilos();
+    document.styleSheets[n].disabled = false;
+}
+
+/*
+ * Permite seleccionar el tema a través del menú dropdown.
+ */
+function seleccionTema() {
+    $(".dropdown-menu li a").click(function () {
+        var seleccion = ($(this).text());
+        localStorage.setItem("tema", seleccion);
+        cargarEstilo();
+    });
+}
